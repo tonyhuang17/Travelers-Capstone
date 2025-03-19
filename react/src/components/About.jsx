dee2d07543d2ac2ab36f7a12ac4ae42420256f00
Oksana Steinke <osteinke@travelers.com>
2025-03-19 16:24:16 -0400

    added About page

--- react/src/components/About.jsx
+++ react/src/components/About.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            <h1>ABOUT PAGE</h1>
+        <div style={{ maxWidth: "700px", margin: "auto", textAlign: "center", fontFamily: "Arial, sans-serif", padding: "20px" }}>
+        <h1 style={{ color: "#d63384" }}>About Glamour Beauty</h1>
+        <p>
+                Welcome to <strong>Glamour Beauty</strong> your go-to destination for high-quality cosmetics.
+                We offer a curated selection of top beauty brands at unbeatable prices.
+        </p>
+
         </div>
     );
 };
