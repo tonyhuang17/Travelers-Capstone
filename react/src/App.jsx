b88038066b2b332bf903d32ef29a664a09c50c67
Tony Huang <anthony.huang@vanderbilt.edu>
2025-03-19 09:55:21 -0500

    move machine

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import item_data from './assets/products.json'
+import item_data from './assets/updated_products.json'
 import Item from "./components/Item"
+import Search from "./components/Search"
 
 function App() {
   return (
     <div>
       <h3>NATURÁL</h3>
+      <Search />
       {
         item_data.map((item) => (
           <Item key = {item.id} data = {item} />
