14736754b256db642592be23300030745ee33ef1
Tony Huang <anthony.huang@vanderbilt.edu>
2025-03-19 09:55:21 -0500

    move machine

---
+++ react/src/components/Search.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+
+const Search = (props) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch(`${import.meta.env.}`)
+    }
+}
